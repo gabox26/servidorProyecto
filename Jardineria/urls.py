4e224cc09b3263dc9ede516108038b3a592dc607
gabox26 <117775433+Gabriel28C@users.noreply.github.com>
2023-05-29 13:29:00 -0400

    commit

--- Jardineria/urls.py
+++ Jardineria/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-import venta
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(venta.urls)), 
 ]
